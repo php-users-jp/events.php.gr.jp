b814ab8719c33814e6b4e93ce2057005e8f54bc9
ha1t <ha1t@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-30 03:03:25 +0000

    isOverメソッドの追加
    git-svn-id: http://svn.coderepos.org/share/websites/events.php.gr.jp/branches/cake@18464 d0d07461-0603-4401-acd4-de1884942a52

--- app/models/event.php
+++ app/models/event.php
@@ -8,6 +8,29 @@ class Event extends AppModel
 {
     var $name = 'Event';
     var $useTable = 'event';
+
+    /**
+     * isOver
+     *
+     */
+    function isOver($event_id, $timestamp = null)
+    {
+        if (is_null($timestamp)) {
+            $timestamp = time();
+        }
+
+        $query = "SELECT due_date FROM event WHERE id = ? LIMIT 1";
+        $result = $this->query($query, array($event_id));
+
+        $duedate = $result[0][0]['due_date'];
+
+        if (strtotime($duedate) < $timestamp) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
 ?>
