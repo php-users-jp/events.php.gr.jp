08981707f74e5fc415f54732d92bf6085ae1bb09
ha1t <ha1t@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-07 18:01:34 +0000

    タイムアウトの設定をのばした
    git-svn-id: http://svn.coderepos.org/share/websites/events.php.gr.jp/branches/cake@18957 d0d07461-0603-4401-acd4-de1884942a52

--- app/config/core.php
+++ app/config/core.php
@@ -126,7 +126,7 @@
  * Session time out time (in seconds).
  * Actual value depends on 'Security.level' setting.
  */
-	Configure::write('Session.timeout', '120');
+	Configure::write('Session.timeout', '3600');
 /**
  * If set to false, sessions are not automatically started.
  */
