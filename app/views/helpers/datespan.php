56373b6d3a9d90b454dc71d89d4ceae5cf4ae50d
ha1t <ha1t@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-09 13:15:25 +0000

    helperにもdoctestを追加
    git-svn-id: http://svn.coderepos.org/share/websites/events.php.gr.jp/branches/cake@19058 d0d07461-0603-4401-acd4-de1884942a52

--- app/views/helpers/datespan.php
+++ app/views/helpers/datespan.php
@@ -14,6 +14,12 @@ class DatespanHelper extends Helper
     /**
      * display
      *
+     * #test
+     * <code>
+     * #eq('2008-09-26 19:00 〜 21:00', #f('2008-09-26 19:00:00', '2008-09-26 21:00:00'));
+     * #eq('2008-09-24 19:00 〜 09-26 21:00', #f('2008-09-24 19:00:00', '2008-09-26 21:00:00'));
+     * #eq('2007-09-24 19:00 〜 2008-09-26 21:00', #f('2007-09-24 19:00:00', '2008-09-26 21:00:00'));
+     * </code>
      */
     function display($start, $end)
     {
@@ -22,12 +28,15 @@ class DatespanHelper extends Helper
 
         $ret = date('Y-m-d H:i', $start);
         $ret.= ' 〜 ';
+
         if (date('Y', $start) != date('Y', $end)) {
             $ret.= date('Y-', $end);
         }
+
         if (date('Y-m-d', $start) != date('Y-m-d', $end)) {
             $ret.= date('m-d ', $end);
         }
+
         $ret.= date('H:i', $end);
 
         return $ret;
